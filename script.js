ce0d4e34f7c6cb78413b6bd09be87c2523b52a33
rroyalty <rroyalty@gmail.com>
2020-12-31 16:03:20 -0500

    Discovered event bubbling.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-
+//Discovered Event bubbling!! Applies on click to my check switches.
+document.addEventListener('click', function (event) {
+  if ( event.target.classList.contains( 'form-check-input' ) ) {
+      validatePW();
+  }
+}, false);
 
 // Assignment Code
 // var generateBtn = document.querySelector("#generate");
@@ -14,6 +19,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Checks to verify that at least one selection is made for symbols. On fail, shows warnings on webpage and disables generate button.
 function validatePW() {
   let checkboxes = document.querySelectorAll('input[name=cbVal]:checked');
   if(checkboxes.length === 0) {
@@ -25,19 +31,15 @@ function validatePW() {
     document.getElementById("generateBtn").disabled = true;
 
   } else {
-    let ele=document.querySelectorAll(".show");
+    let ele = document.querySelectorAll(".show");
     ele.forEach(elem => {
       elem.classList.remove("show");
       elem.classList.add("hidden");
     });
     document.getElementById("generateBtn").disabled = false;
   }
-
 }
+//End validation.
 
 // Add event listener to generate button
   generateBtn.addEventListener("click", writePassword);
-  pwLC.addEventListener("click", validatePW);
-  pwUC.addEventListener("click", validatePW);
-  pwNum.addEventListener("click", validatePW);
-  pwSpec.addEventListener("click", validatePW);
